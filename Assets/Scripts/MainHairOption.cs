ee418aac645b66e72bb441efca0171c65ed9413f
Aidan <103147872+abarciab@users.noreply.github.com>
2025-11-07 15:52:51 -0800

    Manual Hair updates

--- Assets/Scripts/MainHairOption.cs
+++ Assets/Scripts/MainHairOption.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainHairOption : MonoBehaviour
 {
     [SerializeField] private Image _image;
+    [SerializeField] private bool _debug;
+    [SerializeField] private TextMeshProUGUI _debugText;
 
     private FeatureSOData _feature;
     private MainHairController _controller;
@@ -17,6 +20,12 @@ public class MainHairOption : MonoBehaviour
         _feature = feature;
         _image.sprite = feature.Icon;
         _controller = controller;
+
+#if UNITY_EDITOR
+        if (_debug) _debugText.text = feature.name;
+#else
+        Destroy(_debugText);
+#endif
     }
 
     public void Select()
