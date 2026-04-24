6f52f79c61d7c0ba252b6db1ef8b74fb89486647
Aidan <103147872+abarciab@users.noreply.github.com>
2026-04-24 16:59:05 -0700

    Colored Clothing variants!

--- Assets/Scripts/TownSimScripts/ItemData.cs
+++ Assets/Scripts/TownSimScripts/ItemData.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 
 
 public enum ItemType { Clothing, Food, Housing}
+public enum ClothingType { OUTFIT, TOP, BOTTOM }
 
 [CreateAssetMenu(fileName = "Item", menuName = "Item", order = 1)]
 public class ItemData : ScriptableObject
 {
-    public ID ID;
+    [ReadOnly] public ID ID;
 
     public Sprite sprite;
-
     public string Name;
+    [TextArea(3,10)] public string Description;
+    public float Cost;
+    public bool unlocked; //INVESTIGATE
 
+    [Space()]
     public ItemType Type;
 
-    [TextArea]
-    public string Description;
-
-    public float Cost;
-
-    public bool unlocked;
+    [ConditionalField(nameof(Type), false, false, ItemType.Clothing)] public ClothingType ClothingType;
+    [ConditionalField(nameof(Type), false, false, ItemType.Clothing), SerializeField] private bool _customColor;
+    [ConditionalField(nameof(Type), false, false, ItemType.Clothing), SerializeField] private Color _color;
+    
 
     private void OnValidate()
     {
@@ -34,5 +36,12 @@ public class ItemData : ScriptableObject
     {
         ID.GenerateNew();
         Debug.Log("Generating new ID for " + name + ". All saved instances of this item have been erased.");
+        Utils.SetDirty(this);
+    }
+
+    public void AffectMesh(SetMaterialField mesh)
+    {
+        if (!mesh) return;
+        if (_customColor) mesh.SetColor(_color);
     }
 }
