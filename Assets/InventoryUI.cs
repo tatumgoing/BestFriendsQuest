de098e40d40304442fc221eed95e14da038ef48c
Aidan <103147872+abarciab@users.noreply.github.com>
2026-04-25 09:23:05 -0700

    inventory now sorted by number owned

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class InventoryUI : MonoBehaviour, IItemListController
@@ -19,7 +20,7 @@ public class InventoryUI : MonoBehaviour, IItemListController
 
     private void OnEnable()
     {
-        var inventory = TownGameManager.i.GetInventoryItems();
+        var inventory = TownGameManager.i.GetInventoryItems().OrderByDescending(x => TownGameManager.i.GetNumberOwned(x)).ToList();
         _itemListDisplay.DisplayItem(inventory, this);
     }
 
