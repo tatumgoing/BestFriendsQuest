688a7de2e92aae0acaf3a316af0ffd8f387cd84a
Aidan <103147872+abarciab@users.noreply.github.com>
2026-07-08 18:20:49 -0700

    Wip map selector

--- Assets/MapOptionBFQuest.cs
+++ Assets/MapOptionBFQuest.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-[System.Serializable]
-public class MapOptionBFQuestData
-{
-    public string Name;
-    public int NumIslands;
-    public string Difficulty;
-}
 
 public class MapOptionBFQuest : MonoBehaviour
 {
@@ -17,10 +10,11 @@ public class MapOptionBFQuest : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _numIslandsText;
     [SerializeField] private TextMeshProUGUI _difficultyText;
 
-    public void Initiailize(MapOptionBFQuestData data)
+    public void Initiailize(MapData data)
     {
         _nameText.text = data.Name;
         _numIslandsText.text = "Number of Islands: " + data.NumIslands;
-        _difficultyText.text = data.Difficulty;
+        _difficultyText.text = Utils.CapitalFirst(data.Difficulty.ToString());
+        gameObject.SetActive(true);
     }
 }
